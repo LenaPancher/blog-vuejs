612802ef4fa7445b8fae75eb051b8537b37ce2db
Clément Mullet <contact@stacky.fr>
2021-10-13 15:04:38 +0200

    #0.1 Initiation du style

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,54 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="tesla_main_menu" id="nav">
+    <svg class="tds-icon tds-icon-logo-wordmark tds-site-logo-icon" viewBox="0 0 342 35" xmlns="http://www.w3.org/2000/svg"><path d="M0 .1a9.7 9.7 0 007 7h11l.5.1v27.6h6.8V7.3L26 7h11a9.8 9.8 0 007-7H0zm238.6 0h-6.8v34.8H263a9.7 9.7 0 006-6.8h-30.3V0zm-52.3 6.8c3.6-1 6.6-3.8 7.4-6.9l-38.1.1v20.6h31.1v7.2h-24.4a13.6 13.6 0 00-8.7 7h39.9v-21h-31.2v-7h24zm116.2 28h6.7v-14h24.6v14h6.7v-21h-38zM85.3 7h26a9.6 9.6 0 007.1-7H78.3a9.6 9.6 0 007 7zm0 13.8h26a9.6 9.6 0 007.1-7H78.3a9.6 9.6 0 007 7zm0 14.1h26a9.6 9.6 0 007.1-7H78.3a9.6 9.6 0 007 7zM308.5 7h26a9.6 9.6 0 007-7h-40a9.6 9.6 0 007 7z" fill="var(--tds-icon--fill, #171a20)"></path></svg>
+    <div class="tesla_list_menu">
+      <router-link to="/">Accueil</router-link>
+      <router-link to="/about">A propos</router-link>
+      <router-link to="/admin">Administration</router-link>
+    </div>
   </div>
   <router-view/>
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Montserrat;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+body {
+  margin: 0;
 }
+.tesla_main_menu {
+  position: relative;
+  width: 100%;
+  height: 90px;
+  padding: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
-#nav a {
+}
+.tesla_main_menu a {
+  font-size: 16px;
   font-weight: bold;
-  color: #2c3e50;
+  color: #2a2a2a;
+  position: relative;
+  margin: 0 20px;
+  text-decoration: none;
+
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.tesla_main_menu svg {
+  width: 10%;
 }
+
+.tesla_list_menu {
+  padding: 0 20px;
+}
+
 </style>
