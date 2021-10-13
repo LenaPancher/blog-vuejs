d8df37ac70a150024a765764a2427b1756d6bca1
John Doe <lenapancher@gmail.com>
2021-10-13 23:12:26 +0200

    ajout component blogcard + ajout store

--- src/components/BlogCard.vue
+++ src/components/BlogCard.vue
@@ -1,9 +1,23 @@
 <template>
-    <div>Bonjour</div>
+    <div>
+        <div v-for="(info, index) in getDataApi" :key="index">
+          <img :src='info.urlToImage' alt="url-image-article" height="200" width="400">
+          <h5> {{ info.title }}</h5>
+          <h6>Publié le {{ info.publishedAt }}</h6>
+          <p>{{ info.description }}</p>
+          <router-link to="/article" :index="index">Voir l'article</router-link>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-    name: "BlogCard"
+    name: "BlogCard",
+    computed: {
+        getDataApi() {
+          this.$store.dispatch("getData")
+          return this.$store.state.infosArticles
+        }
+    }
 }
 </script>
