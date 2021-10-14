0b4019c7d4027c68dfa5a217e6cbdfce8d9c033f
Lucas Gauvain <lucasgauvain@gmail.com>
2021-10-14 14:08:23 +0200

    ajout de la partie article et routing automatique

--- src/components/BlogCard.vue
+++ src/components/BlogCard.vue
@@ -1,23 +1,32 @@
 <template>
-    <div>
-        <div v-for="(info, index) in getDataApi" :key="index">
-          <img :src='info.urlToImage' alt="url-image-article" height="200" width="400">
-          <h5> {{ info.title }}</h5>
-          <h6>Publié le {{ info.publishedAt }}</h6>
-          <p>{{ info.description }}</p>
-          <router-link to="/article" :index="index">Voir l'article</router-link>
-        </div>
+  <div>
+    <div v-for="(info, index) in getDataApi" :key="index">
+      <img
+        :src="info.urlToImage"
+        alt="url-image-article"
+        height="200"
+        width="400"
+      />
+      <h5>{{ info.title }}</h5>
+      <h6>Publié le {{ info.publishedAt }}</h6>
+      <p>{{ info.description }}</p>
+      <router-link :to="{ name: 'Article', params: { id: index + 1 } }"
+        >Voir l'article</router-link
+      >
     </div>
+  </div>
 </template>
+
 
 <script>
 export default {
-    name: "BlogCard",
-    computed: {
-        getDataApi() {
-          this.$store.dispatch("getData")
-          return this.$store.state.infosArticles
-        }
-    }
-}
+  name: "BlogCard",
+  computed: {
+    getDataApi() {
+      let test = this.$store.state.infosArticles;
+      console.log(this.$store.state.infosArticles);
+      return test;
+    },
+  },
+};
 </script>
