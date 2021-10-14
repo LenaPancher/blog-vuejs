233fcc7d08ab915ace950c047a900e1853720424
Lucas Gauvain <lucasgauvain@gmail.com>
2021-10-14 09:14:06 +0200

    debut ajout de classe

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -9,6 +9,6 @@
 
 <script>
 export default {
-    name: "Navbar"
+    name: "Navbar"  
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,16 @@
 import { createStore } from 'vuex'
 import axios from 'axios'
+import { Article } from '../classes/Article.js'
 
 export default createStore({
   state: {
+    
   },
   mutations: {
   },
   actions: {
     getData(){
-      axios.get("https://newsapi.org/v2/everything?q=tesla&from=2021-09-13&sortBy=publishedAt&apiKey=API_KEY")
+      axios.get(`https://newsapi.org/v2/everything?q=tesla&from=2021-09-13&sortBy=publishedAt&apiKey=45199ce3ce71493c9d8e7b304898433a&pageSize${this.}`)
       .then(data => console.log(data.articles))
     }
   },
