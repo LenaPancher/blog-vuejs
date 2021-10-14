0c2ceaef91764389f27fb1fcbfc51800acb185da
John Doe <lenapancher@gmail.com>
2021-10-14 13:00:47 +0200

    page about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,253 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <main role="main">
+      <div class="panel-panel">
+        <div class="panel-pane pane-page-content">
+          <div class="pane-content">
+            <div class="panel-display ">
+              <section id="article_content" class="panel-panel">
+                <div class="panel-pane pane-custom pane-1">
+                  <div class="pane-content">
+
+                    <header class="section-sub-nav section-about-header">
+                      <div class="container">
+                        <h1 class="section-title section-title-header">À Propos de Tesla</h1>
+                      </div>
+                    </header>
+
+                    <section class="section-about-tesla">
+                      <div class="container">
+                        <div class="section-about">
+                          <h2 class="tesla-mission">La mission de Tesla est d’accélérer la transition mondiale vers un
+                            schéma énergétique durable.</h2>
+                          <div class="about-text">
+                            <p>Tesla a été fondé en 2003 par un groupe d'ingénieurs qui désiraient prouver qu'il était
+                              possible de rouler à bord d'un véhicule électrique sans faire de compromis – que les
+                              véhicules électriques pouvaient être plus performants, plus rapides et plus agréables à
+                              conduire que les véhicules à combustion classiques. Aujourd'hui, Tesla construit des
+                              véhicules 100% électriques, mais également des solutions extensibles de production et de
+                              stockage d'énergie nouvelle génération. Pour Tesla, au plus vite l'humanité s'affranchira
+                              des énergies fossiles au profit d'énergies renouvelables, au mieux le monde se
+                              portera.</p>
+                            <p>Lancé en 2008, le Tesla Roadster embarquait une batterie et un groupe motopropulseur
+                              électrique totalement révolutionnaires. Tesla a par la suite intégralement mis au point, à
+                              partir d'une feuille blanche, la toute première berline haut de gamme 100 % électrique :
+                              la <a href="/fr_FR/models">Model S.</a> Elle est devenue depuis, la meilleure de sa
+                              catégorie, toutes classes confondues. Combinant performances, sécurité et efficacité, la
+                              Model S a révolutionné la perception du véhicule du 21e siècle, en offrant la plus grande
+                              autonomie disponible sur un véhicule électrique, des mises à jour à distance améliorant
+                              continuellement les capacités du véhicule tout au long de son cycle de vie, ainsi qu'une
+                              accélération record de 0 à 60 mph en 2,28 secondes, mesurée par le magazine Motor Trend.
+                              En 2015, Tesla étend sa gamme avec le <a href="/fr_FR/modelx">Model X</a>, le SUV le plus
+                              sûr, le plus rapide et le plus polyvalent de l'histoire. Il est le premier SUV à avoir
+                              obtenu une note de 5 étoiles dans toutes les catégories du crash-test de la National
+                              Highway Traffic Safety Administration (NHTSA). Pour compléter le <a
+                                  href="/fr_FR/blog/secret-tesla-motors-master-plan-just-between-you-and-me">« Secret
+                                Master Plan »</a> rédigé par Elon Musk, Tesla dévoile la <a href="/fr_FR/model3">Model
+                                3</a> en 2016. Un véhicule électrique abordable et de grande série dont la production a
+                              débuté en 2017. Peu après, Tesla présente le camion le plus sûr et le plus confortable de
+                              l'histoire, le <a href="/fr_FR/semi">Tesla Semi</a>. Ce dernier a été conçu pour faire
+                              économiser à ses propriétaires au moins 200 000 $ sur plus d'un million de miles
+                              parcourus, rien qu'en dépenses de carburant. En 2019, Tesla dévoile le <a
+                                  href="/fr_FR/modely">Model Y</a>, un SUV de taille intermédiaire pouvant accueillir
+                              jusqu'à sept passagers, et le <a href="/fr_FR/cybertruck">Cybertruck</a>, plus polyvalent
+                              qu'un pick-up traditionnel et plus performant qu'une voiture de sport.</p>
+                            <p>Les véhicules Tesla sont produits dans l'<a href="/fr_FR/factory">usine</a> de Fremont,
+                              en Californie, et dans la Gigafactory de Shanghai. Afin d'atteindre notre objectif visant
+                              à posséder les usines les plus sûres au monde, Tesla adopte une approche proactive quant à
+                              la sécurité et demande aux employés de production de participer à un programme de
+                              formation de plusieurs jours avant d'entrer dans l'usine. A partir de là, Tesla continue
+                              de proposer une formation par tâche et suit le rendement chaque jour afin que des
+                              améliorations puissent être apportées rapidement. Ainsi, le taux de sécurité de Tesla
+                              continue de s'améliorer à mesure que la production s'accélère.</p>
+                            <p>Pour mettre en place un écosystème énergétique durable, Tesla produit également un
+                              éventail unique de solutions énergétiques. Le <a href="/fr_FR/powerwall">Powerwall</a>, le
+                              <a href="/fr_FR/powerpack">Powerpack</a> et le <a href="/fr_FR/solarroof">Solar Roof</a>
+                              permettent aux particuliers et aux professionnels de maîtriser leur production, stockage
+                              et consommation d'énergie renouvelable. C'est dans le but de soutenir les gammes
+                              automobiles et énergétiques de Tesla qu'a été créée la <a href="/fr_FR/gigafactory">Gigafactory
+                                1</a>, une usine créée pour réduire de manière significative le coût des cellules. En
+                              internalisant la production de cellules, Tesla fabrique des batteries en quantité
+                              nécessaire pour atteindre ses objectifs de production, créant dans le même temps des
+                              milliers d'emplois.</p>
+                            <p>Et ce n'est que le début. En produisant son véhicule le plus accessible actuellement,
+                              Tesla continue d'offrir des produits de plus en plus abordables et accessibles au plus
+                              grand nombre, accélérant ainsi le développement du transport propre et de la production
+                              d'énergie renouvelable. Les véhicules électriques, les batteries ainsi que les solutions
+                              de production et de stockage d'énergie renouvelable existent séparément, mais lorsqu'ils
+                              sont associés, ils deviennent encore plus performants. C'est le futur que nous
+                              souhaitons.</p>
+                          </div>
+                        </div>
+
+                        <div class="section-press">
+                          <div class="press-contacts">
+                            <h3 class="section-title press-contacts-title">Presse</h3>
+                            <address class="vcard">
+                              <div class="region">Amérique du nord</div>
+                              <div class="email"><a href="mailto:press@tesla.com">Press@tesla.com</a></div>
+                            </address>
+                            <address class="vcard">
+                              <div class="region">Europe</div>
+                              <div class="email"><a href="mailto:eupress@tesla.com">EUPress@tesla.com</a></div>
+                            </address>
+                            <address class="vcard">
+                              <div class="region">Australie et Asie</div>
+                              <div class="email"><a href="mailto:APACPress@tesla.com">APACPress@tesla.com</a></div>
+                            </address>
+                            <address class="vcard">
+                              <div class="region">Chine</div>
+                              <div class="email"><a href="mailto:china-press@tesla.com">China-Press@tesla.com</a></div>
+                            </address>
+                          </div>
+                        </div>
+
+                      </div>
+                    </section>
+
+                  </div>
+                </div>
+              </section>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
+
+<style scoped>
+  #article_content{
+    margin-top: 100px;
+  }
+
+  .container{
+    margin-left: 200px;
+    margin-right: 200px;
+  }
+
+  .section-title{
+    border-bottom: 1px solid #5c5e62;
+    font-size: 30px;
+    margin-bottom: 35px;
+    padding-bottom: 25px;
+    position: relative;
+    top: 0;
+    font-weight: normal;
+  }
+
+  .section-about-tesla{
+    margin-bottom: 30px;
+    color: #393c41;
+    line-height: 23px;
+  }
+
+  .section-about{
+    width: 60%;
+    float: left;
+    position: relative;
+  }
+
+  .tesla-mission{
+    font-weight: bold;
+    font-size: 30px;
+    line-height: 40px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+
+  .about-text{
+    margin-top: 30px;
+  }
+
+  .section-press{
+    width: 35%;
+    float: right;
+  }
+
+  .press-contacts{
+    margin-left: 30px;
+  }
+
+  .press-contacts-title{
+    font-weight: 500;
+    font-size: 16px;
+    padding-top: 0;
+  }
+
+  .vcard{
+    margin-bottom: 30px;
+    font-style: normal;
+    color: #5c5e62;
+  }
+
+  .region, .email{
+    font-size: 15px;
+  }
+
+  a{
+    color: #393c41;
+  }
+
+  @media screen and (max-width: 640px) {
+    main {
+      width: auto;
+    }
+
+    .container{
+      margin-left: auto;
+      margin-right: auto;
+      max-width: 1060px;
+      padding-left: 20px;
+      padding-right: 20px;
+      position: relative;
+      display: flex;
+      flex-direction: column;
+    }
+
+    .section-about{
+      width: auto;
+    }
+
+  }
+
+  @media screen and (max-width: 960px) {
+    main {
+      width: auto;
+    }
+  }
+
+  @media screen and (max-width: 1024px) {
+    main {
+      width: auto;
+    }
+
+    .container{
+      margin-left: auto;
+      margin-right: auto;
+      max-width: 1060px;
+      padding-left: 20px;
+      padding-right: 20px;
+      position: relative;
+      display: flex;
+      flex-direction: column;
+    }
+
+    .section-about{
+      width: auto;
+    }
+
+    .section-title{
+      font-size: 23px;
+    }
+
+    .section-press{
+      width: 100%;
+    }
+
+    .press-contacts{
+      margin-left: 0;
+    }
+  }
+
+</style>
