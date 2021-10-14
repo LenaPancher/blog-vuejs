da75f2f59d1812640d204531dc7c5d2a2bba1103
Lucas Gauvain <lucasgauvain@gmail.com>
2021-10-14 09:29:46 +0200

    minor typos

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-import {createStore} from 'vuex'
+import { createStore } from 'vuex'
 import axios from 'axios'
 
 export default createStore({
@@ -7,26 +7,26 @@ export default createStore({
         infosArticles: []
     },
     mutations: {
-        updateInfosArticles(state, article) {
-            state.infosArticles.push(article)
+        updateInfosArticles(state, articles) {
+            state.infosArticles.push(articles)
         }
     },
     actions: {
         getData(context) {
-            axios.get(`https://newsapi.org/v2/everything?q=tesla&from=2021-09-13&sortBy=publishedAt&apiKey=45199ce3ce71493c9d8e7b304898433a&pageSize=${this.nbArticles}&language=en`)
+            axios.get(`https://newsapi.org/v2/everything?q=tesla&sortBy=publishedAt&apiKey=45199ce3ce71493c9d8e7b304898433a&pageSize=${this.nbArticles}&language=fr`)
                 .then(result => {
-                        for (let i = 0; i <= context.state.nbArticles; i++) {
-                            let article = {
-                                "title": result.data.articles[i].title,
-                                "publishedAt": new Date(result.data.articles[i].publishedAt).toLocaleDateString(),
-                                "author": result.data.articles[i].author,
-                                "description": result.data.articles[i].description,
-                                "urlToImage": result.data.articles[i].urlToImage,
-                                "content": result.data.articles[i].content,
-                            }
-                            context.commit('updateInfosArticles', article);
+                    for (let i = 0; i <= context.state.nbArticles; i++) {
+                        let articles = {
+                            "title": result.data.articles[i].title,
+                            "publishedAt": new Date(result.data.articles[i].publishedAt).toLocaleDateString(),
+                            "author": result.data.articles[i].author,
+                            "description": result.data.articles[i].description,
+                            "urlToImage": result.data.articles[i].urlToImage,
+                            "content": result.data.articles[i].content,
                         }
+                        context.commit('updateInfosArticles', articles);
                     }
+                }
                 )
         }
     },
